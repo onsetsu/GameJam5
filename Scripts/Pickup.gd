9b160247ebdaed0d43186d6f70750f204cd3e349
Leon Matthes <leon@matthes.biz>
2018-11-30 22:32:51 +0100

    New version of player can now pickup and throw stuff

--- Scripts/Pickup.gd
+++ Scripts/Pickup.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var throwImpulse = 100
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -20,5 +21,13 @@ func _on_PickupArea_body_entered(body):
 		body.pick_up(self)
 		
 func picked_up_by(player):
-	$PickupArea.monitoring = false
+	print('picked up')
+	$PickupArea.set_monitoring(false)
+	self.set_linear_velocity(Vector2(0,0))
+	self.set_angular_velocity(0)
 	get_parent().remove_child(self)
+	set_position(Vector2(0,0))
+	
+func thrown_by(player, direction):
+	$PickupArea.set_monitoring(false)
+	self.apply_impulse(to_global(Vector2(0,0)), direction * throwImpulse)
